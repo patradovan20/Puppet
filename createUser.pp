18b2d837d1282598e3efb8565e6065e131aa9e23
patrdvn <patricia.radovan@gmail.com>
2018-02-16 06:30:07 +0800

    Added managehome and system

--- createUser.pp
+++ createUser.pp
@@ -3,9 +3,11 @@
 class create_user{
 	user { 'monitor':
   		ensure           => 'present',
- 		home             => "/home/monitor",
-  		password         => 'password1',
+ 		home             => '/home/monitor',
  		shell            => '/bin/bash',
+		password	 => '$1$2fZbll.F$6H4UZA0O/fxU2K57EhfXf0',
+		managehome	 => true,
+		system		 => true,
 	      }
 
 }
